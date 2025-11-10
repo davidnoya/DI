da2b8dd8b4d53f73ee3443e89d62418499092b39
David Noya <dnoyav23@fpcoruna.afundacion.org>
2025-11-10 17:00:44 +0100

    Sprint 3 Android - Tarea 2

---
+++ sprint3android/CatalogActivity/app/src/main/java/com/example/catalogactivity/CatalogActivity.java
@@ -0,0 +1,27 @@
+package com.example.catalogactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botonDetalle = findViewById(R.id.botonDetalle);
+
+        botonDetalle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
